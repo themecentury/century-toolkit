a2e51ae7e8adfcc2529922e71df8ed502a97173c
themecentury <themecentury@gmail.com>
2019-03-23 12:56:54 +0545

    newspaper plugin demo implemented

--- includes/theme-demo/class-tcy-theme-demo-newspaper-plus.php
+++ includes/theme-demo/class-tcy-theme-demo-newspaper-plus.php
@@ -8,14 +8,14 @@ class TCY_Theme_Demo_Newspaper_Plus extends TCY_Theme_Demo {
 
 	public static function import_files() {
 
-		$demo_server_url = 'https://raw.githubusercontent.com/themecentury/demo/master/newspaper-plus/default/';
+		$demo_server_url = 'https://raw.githubusercontent.com/themecentury/demo/master/newspaper-plus/';
 		$demo_url = 'https://demo.themecentury.com/wpthemes/newspaper-plus/';
 		$demo_urls  = array(
 			array(
 				'import_file_name'           => 'Newspaper Plus',
-				'import_file_url'            => $demo_server_url . 'content.xml',
-				'import_widget_file_url'     => $demo_server_url . 'widgets.wie',
-				'import_customizer_file_url' => $demo_server_url . 'customizer.dat',
+				'import_file_url'            => $demo_server_url . 'default/content.xml',
+				'import_widget_file_url'     => $demo_server_url . 'default/widgets.wie',
+				'import_customizer_file_url' => $demo_server_url . 'default/customizer.dat',
 				'import_preview_image_url'   => 'https://i0.wp.com/themes.svn.wordpress.org/newspaper-lite/1.0.4/screenshot.png',
 				'demo_url'                   => $demo_url . '',
 				//'import_notice'              => __( 'After you import this demo, you will have to setup the slider separately.', 'your-textdomain' ),
